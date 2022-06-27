c656ecf8159daa4a341dff21f334a02e3a518243
Bruno <cingolanib@gmail.com>
2022-06-27 14:33:48 -0300

    se agrego cors y nuevos paquetes para la parte de imagenes y mails

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,6 @@ router.post('/login', userController.loginUser)
 //router.get('/me', protect, userController.getMe)
 router.get('/me', protect, userController.getUserByToken)
 router.post('/email', protect, userController.getUserByEmail)
-router.route("/").put(protect,userController.updateUser)//ver video clase donde sacan el id
+router.route("/update").put(protect,userController.updateUser)
 
 module.exports = router
